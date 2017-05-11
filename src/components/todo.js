49c72079051660ef63ab36da85a71aaf79be874a
FuminaMIYAHARA <fumina.miya@gmail.com>
2017-05-11 16:42:51 +0900

    get drag target using ref

--- src/components/todo.js
+++ src/components/todo.js
@@ -3,12 +3,19 @@ import PropTypes from 'prop-types';
 
 class Todo extends React.Component {
     render() {
+        let element;
         // propとして渡って来たtextを表示するだけ
         return (
             <li style={{backgroundColor: this.props.drag ? 'red' : 'white'}}
             onDragStart={() => {this.props.onDragStart(this.props.id)}}
-            onDragEnd={() => {this.props.onDragEnd(this.props.id)}}
-            draggable="true">
+            onDragEnd={() => {
+                this.props.onDragEnd(this.props.id);
+                console.log(element);
+            }}
+            draggable="true"
+            ref={(node) => {
+                element = node;
+            }}>
                 {this.props.text}
             </li>
         );
